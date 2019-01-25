2cef3695a5e0113ffface303ad86c26bbc889de0
Bogdan Anghelache <bogdananghelache1097@gmail.com>
2019-01-25 21:20:29 +0200

    Added game loop, piece and filed drawing and collision detection

--- source/source.cpp
+++ source/source.cpp
@@ -1,64 +1,32 @@
 #include "pch.h"
 #include <iostream>
 #include <Windows.h>
+#include <thread>
 using namespace std;
 
 wstring tetromino[7];
-int nFieldWidth = 12;
+int nFieldWidth = 20;
 int nFieldHeight = 18;
 unsigned char * pField = nullptr;
 
-int nScreenWidth = 80;		// Console Screen Size width	
+int nScreenWidth = 120;		// Console Screen Size width	
 int nScreenHeight = 30;		// Console Screen Size height
 
-int Rotate(int px, int py, int r) {
-	switch (r % 4) {
-	case 0: return py * 4 + px;			// 0 degrees
-	case 1: return 12 + py - py * 4;	// 90 degrees
-	case 2: return 15 - py * 4 - px;	// 180 degrees
-	case 3: return 3 - py + px * 4;		// 270 degrees
-	}
+int nDrawOffset = 2;
 
-	return 0;
-}
+int Rotate(int px, int py, int r);
+bool DoesPieceFit(int nTetromino, int nRotation, int nPosX, int nPosY);
 
 int main()
 {
 	//create assets
-	tetromino[0].append(L"..X.");
-	tetromino[0].append(L"..X.");
-	tetromino[0].append(L"..X.");
-	tetromino[0].append(L"..X.");
-
-	tetromino[1].append(L".XX.");
-	tetromino[1].append(L"..X.");
-	tetromino[1].append(L"..X.");
-	tetromino[1].append(L"....");
-
-	tetromino[2].append(L".XX.");
-	tetromino[2].append(L".X..");
-	tetromino[2].append(L".X..");
-	tetromino[2].append(L".X..");
-
-	tetromino[3].append(L"....");
-	tetromino[3].append(L".XX.");
-	tetromino[3].append(L".XX.");
-	tetromino[3].append(L"....");
-
-	tetromino[4].append(L"..X.");
-	tetromino[4].append(L".XX.");
-	tetromino[4].append(L"..X.");
-	tetromino[4].append(L"....");
-
-	tetromino[5].append(L"..X.");
-	tetromino[5].append(L".XX.");
-	tetromino[5].append(L".X..");
-	tetromino[5].append(L"....");
-
-	tetromino[6].append(L".X..");
-	tetromino[6].append(L".XX.");
-	tetromino[6].append(L"..X.");
-	tetromino[6].append(L"....");
+	tetromino[0].append(L"..X...X...X...X.");
+	tetromino[1].append(L".XX...X...X.....");
+	tetromino[2].append(L".XX..X...X...X..");
+	tetromino[3].append(L".....XX..XX.....");
+	tetromino[4].append(L"..X..XX...X.....");
+	tetromino[5].append(L"..X..XX..X......");
+	tetromino[6].append(L".X...XX...X.....");
 
 	pField = new unsigned char[nFieldHeight * nFieldWidth]; // Create array
 
@@ -76,20 +44,127 @@ int main()
 	DWORD dwBytesWritten = 0;
 
 
+	// Game Logic 
 	bool bGameOver = false;
 
+	int nCurrentPiece = 6;
+	int nCurrentRotation = 0;
+	int nCurrentX = nFieldWidth / 2;
+	int nCurrentY = 0;
+
+	bool bKey[4]; //Right, Left, Down, Z
+
+
 	//Game loop
-	while (!bGameOver) {
+	while (!bGameOver) 
+	{	
+		// GAME TIMING ==============================
+		this_thread::sleep_for(50ms);
+
+		// INPUT ====================================
+		for (int k = 0; k < 4; k++)
+		{														// R   L    D Z
+			bKey[k] = (0x8000 & GetAsyncKeyState((unsigned char)("\x27\x25\x28Z"[k]))) != 0;
+		}
+		 
+		// GAME LOGIC ===============================
+
+		//Right arrow pressed
+		if (bKey[0])
+		{
+			if (DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX + 1, nCurrentY))
+			{
+				nCurrentX = nCurrentX + 1;
+			}
+		}
+
+		//Left arrow pressed
+		if (bKey[1])
+		{
+			if (DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX - 1, nCurrentY))
+			{
+				nCurrentX = nCurrentX - 1;
+			}
+		}
+
+		//Down arrow pressed
+		if (bKey[2])
+		{
+			if (DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX, nCurrentY - 1))
+			{
+				nCurrentY = nCurrentY + 1;
+			}
+		}
+
+		//Z key pressed
+		if (bKey[3])
+		{
+			/*if (DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX - 1, nCurrentY))
+			{
+				nCurrentX = nCurrentX - 1;
+			}*/
+		}
+
+
+
+		// RENDER OUTPUT  ============================
 
 		//Draw field
 		for (int x = 0; x < nFieldWidth; x++) {
 			for (int y = 0; y < nFieldHeight; y++) {
-				screen[(y+2) * nScreenWidth + (x+2)] = L" ABCDEFG=#"[pField[y*nFieldWidth + x]]; //cool trick
+				screen[(y + nDrawOffset) * nScreenWidth + (x + nDrawOffset)] = L" ABCDEFG=#"[pField[y*nFieldWidth + x]]; //cool trick
+				
 			}
 		}
+
+		//Draw Current Piece
+		for (int px = 0; px < 4; px++)
+			for (int py = 0; py < 4; py++)
+			{
+				if (tetromino[nCurrentPiece][Rotate(px, py, nCurrentRotation)] == L'X')
+					screen[(nCurrentY + py + nDrawOffset) * nScreenWidth + (nCurrentX + px + nDrawOffset)] = nCurrentPiece + 65;
+			}
 		//Display frame
 		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
 
 	}
 
+}
+
+int Rotate(int px, int py, int r) 
+{
+	switch (r % 4) {
+	case 0: return py * 4 + px;			// 0 degrees
+	case 1: return 12 + py - py * 4;	// 90 degrees
+	case 2: return 15 - py * 4 - px;	// 180 degrees
+	case 3: return 3 - py + px * 4;		// 270 degrees
+	}
+
+	return 0;
+}
+
+bool DoesPieceFit(int nTetromino, int nRotation, int nPosX, int nPosY)
+{
+	// iterate through each position of the tetromino
+	for(int px = 0; px < 4; px ++)
+		for (int py = 0; py < 4; py++)
+		{
+			// Get index into piece
+			int pi = Rotate(px, py, nRotation);
+
+			// Get index into field
+			int fi = (nPosY + py) * nFieldWidth + (nPosX + px);
+
+			//Bounds
+			if (nPosX + px >= 0 && nPosX + px < nFieldWidth)
+			{
+				if (nPosY + py >= 0 && nPosY+ py < nFieldHeight)
+				{
+					if (tetromino[nTetromino][pi] == L'X' && pField[fi] != 0)
+						return false; //fail, piece can't exist on a non-empty space on the field 
+				}
+			}
+		}
+
+	return true;
 }
